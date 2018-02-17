a60ffc2e20286482303a5b9148bd78f68966c266
melishatissera <melishatissera@gmail.com>
2018-02-17 23:08:08 +0530

    #invoice_canel

--- inventory/frmInvoiceCancellation.cs
+++ inventory/frmInvoiceCancellation.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace inventory
 {
@@ -16,5 +17,39 @@ namespace inventory
             InitializeComponent();
         }
 
+
+        public void generateLastInvoiceID()
+        {
+            long value;
+            SqlConnection con = connection.OpenConnection();
+            SqlCommand cmd = new SqlCommand("SELECT TOP 1 [ItmId] FROM [dbo].[TblItem] WHERE [InvActive]==0 ORDER BY [ItmId] DESC", con);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            if (rdr.HasRows)
+            {
+                rdr.Read();
+                value = rdr.GetInt64(0);
+                lblLastCancelledInv.Text = value.ToString();
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int InvoiceNo = int.Parse(txtInvoiceNo.Text);
+            SqlConnection con = connection.OpenConnection();
+            SqlCommand cmd = new SqlCommand("Update  [dbo].[TblItem] Set [InvActive]==0 WHERE [ItmId]==@ItmId", con);
+            cmd.Parameters.AddWithValue("@ItmId", InvoiceNo);
+            cmd.ExecuteNonQuery();
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Invoice Cancelled Successfuly.");
+            }
+            else
+            {
+                MessageBox.Show("An Error Occured.");
+            }
+        }
     }
 }
